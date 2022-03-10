b3af050172ca62c687fa64507e3ccfc724570cc4
Mohammad Rahhal <m.r992@hotmail.com>
2022-03-10 21:16:45 +0200

    Document more

--- src/charset.rs
+++ src/charset.rs
@@ -13,6 +13,7 @@ const FULL_WIDTH_ROMAN_HALF_WIDTH_KATAKANA_END: char = '\u{ffef}';
 const KANJI_START: char = '\u{4e00}';
 const KANJI_END: char = '\u{9faf}';
 
+/// Returns `true` if the `char` is Japanese (kana, kanji, Japanese punctuation, etc).
 pub fn is_japanese(ch: char) -> bool {
     is_hiragana(ch)
         || is_kanji(ch)
@@ -21,23 +22,30 @@ pub fn is_japanese(ch: char) -> bool {
         || is_full_width_roman_half_width_katakana(ch)
 }
 
+/// Returns `true` if the `char` is a Japanese character (kana, kanji).
 pub fn is_japanese_character(ch: char) -> bool {
     is_hiragana(ch) || is_kanji(ch) || is_katakana(ch) || is_japanese_special_character(ch)
 }
 
-/// Detects if the char is a Japanese punctuation (3000 - 303f).
+/// Returns `true` if the `char` is a Japanese punctuation (3000 - 303f).
 pub fn is_japanese_punctuation(ch: char) -> bool {
     ch >= PUNCTUATION_START && ch <= PUNCTUATION_END
 }
 
+/// Returns `true` if the `char` is a Japanese special character ('々' | '〆').
 pub fn is_japanese_special_character(ch: char) -> bool {
-    ch == '々' || ch == '〆'
+    match ch {
+        '々' | '〆' => true,
+        _ => false,
+    }
 }
 
+/// Returns `true` if the `char` is a hiragana character.
 pub fn is_hiragana(ch: char) -> bool {
     ch >= HIRAGANA_START && ch <= HIRAGANA_END
 }
 
+/// Returns `true` if the string is all hiragana.
 pub fn is_hiragana_string(str: &str) -> bool {
     for ch in str.chars() {
         if !is_hiragana(ch) {
@@ -47,10 +55,12 @@ pub fn is_hiragana_string(str: &str) -> bool {
     true
 }
 
+/// Returns `true` if the `char` is a katakana character.
 pub fn is_katakana(ch: char) -> bool {
     ch >= KATAKANA_START && ch <= KATAKANA_END && ch != '・'
 }
 
+/// Returns `true` if the string is all katakana.
 pub fn is_katakana_string(str: &str) -> bool {
     for ch in str.chars() {
         if !is_katakana(ch) {
@@ -60,15 +70,18 @@ pub fn is_katakana_string(str: &str) -> bool {
     true
 }
 
+/// Returns `true` if the `char` is a full width roam or half width katakana character.
 pub fn is_full_width_roman_half_width_katakana(ch: char) -> bool {
     ch >= FULL_WIDTH_ROMAN_HALF_WIDTH_KATAKANA_START
         && ch <= FULL_WIDTH_ROMAN_HALF_WIDTH_KATAKANA_END
 }
 
+/// Returns `true` if the `char` is a kana character (either hiragana or katakana).
 pub fn is_kana(ch: char) -> bool {
     is_hiragana(ch) || is_katakana(ch)
 }
 
+/// Returns `true` if the `char` is a kanji character.
 pub fn is_kanji(ch: char) -> bool {
     ch >= KANJI_START && ch <= KANJI_END
 }
